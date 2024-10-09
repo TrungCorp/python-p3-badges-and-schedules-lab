258cff39d9d341ff94ef3e31a70afba28e21f30c
TrungCorp <trungle9995@gmail.com>
2024-10-09 02:42:42 -0400

    complete lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+    name_string = f'Hello, my name is {name}.'
+    return name_string
 
 def batch_badge_creator(names):
-    return None
+    list_obj = []
+    for name in names:
+        string_obj = f"Hello, my name is {name}."
+        list_obj.append(string_obj)
+    return list_obj
 
 def assign_rooms(names):
-    return None
+    list_obj = []
+    counter = 1
+    for name in names:
+        string_obj = f"Hello, {name}! You'll be assigned to room {counter}!"
+        list_obj.append(string_obj)
+        counter += 1
+    return list_obj
 
 def printer(names):
+    counter = 1
+    for name in batch_badge_creator(names):
+        print(name)
+    for name in assign_rooms(names):
+        print(name)
     return None
